fee1c46f1d083c1157c18f8de745284edbd08e7a
jiangbl-lj <64793028+jiangbl-lj@users.noreply.github.com>
2020-05-26 17:20:42 +0800

    修改model user api

--- webapp/views.py
+++ webapp/views.py
@@ -2,29 +2,38 @@ from django.http import JsonResponse
 from rest_framework import viewsets
 from rest_framework.parsers import JSONParser
 
+
 from rest_framework.views import APIView
 
 from webapp.models import User
-from webapp.serializers import Test, UserSerializer
-from webapp.serializers import Testserializers
+from webapp.serializers import Apilistserializers, Userserializers,Apiparaterserializers
 from requests import request
+
 
 
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = Userserializers
 
-def test(request):
+def user(request):
     if request.method=='Get':
-        test_obj=Test.objects.all()
-        serializer=Testserializers(test_obj,many=True)
+        user_obj=User.objects.all()
+        serializer=Userserializers(user_obj,many=True)
         return JsonResponse(serializer.data,save=False)
     elif request.method=='Post':
         data=JSONParser.parse(request)
-        serializer=Testserializers(data=data)
+        serializer=Userserializers(data=data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data, status=300)
-        return JsonResponse(serializer.data, status=501)
+        return JsonResponse(serializer.data, status=200)
+
+
+def getapilists():
+    url='http://qa2.roadefend.com/webng/v2/api-docs'
+    res.=request('GET',url=url)
+
+
+
